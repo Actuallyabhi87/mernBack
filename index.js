96890d74fcf3c43c2f520408ecb5650feeb820a6
Abhishek Kashyap <100584650+Actuallyabhi87@users.noreply.github.com>
2024-01-17 17:30:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const app = express();
 const path = require("path");
 
 const port = 5000;
-// app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World!"));
 app.use(cors());
 app.use(express.json());
 app.use(bodyparser.json());
 app.use("/api", require("./Routes/CreateUser"));
 app.use("/api", require("./Routes/DisplayData"));
 app.use("/api", require("./Routes/OrderData"));
-app.use(express.static(path.join(__dirname, "./build")));
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./build/index.html"));
-});
+// app.use(express.static(path.join(__dirname, "./build")));
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./build/index.html"));
+// });
 
 // app.get("/", (req, res) => {
 //   app.use(express.static(path.resolve(__dirname, "client", "build")));
